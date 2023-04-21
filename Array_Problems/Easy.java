4355d4b1cff04846363b3e83e44ef7289b4868ae
bsoumya746 <bsoumya746@gmail.com>
2023-04-21 17:11:52 +0530

    Updated

---
+++ Array_Problems/Easy.java
@@ -0,0 +1,43 @@
+/* Largest Element in an Array. */
+/*
+import java.util.*;
+public class Easy{
+
+    public static int maximum(int arr[], int size){
+        int max=arr[0];
+        for (int i=1;i<size;i++){
+            if(max<arr[i]){
+                max=arr[i];
+            }
+        }
+
+        return max;
+
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of elements in your array:");
+        int size = sc.nextInt();
+
+        int arr[] = new int[size];
+
+        System.out.println("Enter the elements:");
+        for (int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        
+        int max = maximum(arr,size);
+
+        System.out.println("The largest element in the array is:"+max);
+        sc.close();
+    }
+}
+
+ */
+
+
+
+/* Second Largest elemenet in an array without sorting */
+ 
--- Sorting/Quick_Sort.java
+++ Sorting/Quick_Sort.java
@@ -1,2 +1,80 @@
 /* Code for Quick-Sorting */
 
+/* 
+
+import java.util.*;
+
+class Quick_Sort{
+
+    public static int partition (ArrayList<Integer> arr, int low, int high){
+        int pivot= arr.get(low);
+        int i=low;
+        int j=high;
+
+        while(i<j){
+            while(arr.get(i) <= pivot && i <= (high-1)){
+                i++;
+            }
+
+            while(arr.get(j) > pivot && j >= (low+1)){
+                j--;
+            }
+
+            if(i<j){
+                int temp=arr.get(i);
+                arr.set(i, arr.get(j));
+                arr.set(j,temp);
+            }
+        }
+        int temp = arr.get(low);
+        arr.set(low, arr.get(j));
+        arr.set(j, temp);
+        return j;
+    }
+
+    public static void sorting( ArrayList<Integer> arr, int low, int high){
+        if (low < high){
+            int p_Ind = partition(arr, low, high);
+            sorting(arr,low,(p_Ind-1));
+            sorting(arr,(p_Ind+1),high);
+        }
+    }
+
+    public static ArrayList<Integer> qSort( ArrayList<Integer> arr){
+        sorting(arr,0,(arr.size()-1));
+        return arr;
+    }
+
+    public static void main(String args[]){
+        
+        Scanner sc = new Scanner (System.in);
+
+        System.out.println("Enter the number of elements in your array:");
+        int n = sc.nextInt();
+
+        ArrayList <Integer> arr = new ArrayList<>(); 
+
+        System.out.println("Enter the elements:");
+        for (int i=0;i<n;i++){
+            arr.add(i, sc.nextInt());
+        }
+        
+
+        System.out.println("Before Using insertion Sort: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr.get(i) + " ");
+        }
+        
+        System.out.println();
+        arr=qSort(arr);
+
+        System.out.println("After insertion sort: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr.get(i) + " ");
+        }
+        System.out.println();
+        sc.close();
+    }
+}
+
+*/
