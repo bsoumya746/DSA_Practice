0e69de06a4af4e57d61a55e76e2cad8d94c3399c
bsoumya746 <bsoumya746@gmail.com>
2023-04-21 15:37:53 +0530

    changes updated

--- Sorting/Sorting_Basics.java
+++ Sorting/Sorting_Basics.java
@@ -55,15 +55,110 @@ class Sorting{
 
 /* *****Code for selection-sorting***** */
 
+/* 
+import java.util.*;
+
+public class Sorting_Basics {
+    static void selection_sort(int arr[], int n) {
+        for (int i = 0; i < n - 1; i++) {
+            int mini = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[mini]) {
+                    mini = j;
+                }
+            }
+            //swap
+            int temp = arr[mini];
+            arr[mini] = arr[i];
+            arr[i] = temp;
+        }
+
+        System.out.println("After selection sort:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+
+		System.out.println("Enter the number of elements in your array:");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+        
+		int arr[] = new int[n];
+
+		System.out.println("Enter the elements in your array:");
+		for (int i=0;i<n;i++){
+			arr[i]=sc.nextInt();
+		}
+        
+		System.out.println("Before selection sort:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    	selection_sort(arr, n);
+		sc.close();
+	}
+
+	
+}
 
 
+*/
 
 
+/* *****Code for insertion-sorting***** */
+
+/* 
+import java.util.*;
+
+public class Sorting_Basics {
+    static void insertion_sort(int[] arr, int n) {
+        for (int i = 0; i <= n - 1; i++) {
+            int j = i;
+            while (j > 0 && arr[j - 1] > arr[j]) {
+                int temp = arr[j - 1];
+                arr[j - 1] = arr[j];
+                arr[j] = temp;
+                j--;
+            }
+        }
+
+        System.out.println("After insertion sort: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String args[]) {
+        
+		System.out.println("Enter the number of elements in your array:");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+        
+		int arr[] = new int[n];
+
+		System.out.println("Enter the elements in your array:");
+		for (int i=0;i<n;i++){
+			arr[i]=sc.nextInt();
+		}
+
+        System.out.println("Before Using insertion Sort: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        insertion_sort(arr, n);
+
+		sc.close();
+    }
+
+} 
 
 
-
-
-
+*/
 
 
 
