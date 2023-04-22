efdb6a91d903844ed091343a1ab4c4d6f385ac69
bsoumya746 <bsoumya746@gmail.com>
2023-04-23 00:11:35 +0530

    Updated

--- Array_Problems/Easy.java
+++ Array_Problems/Easy.java
@@ -100,4 +100,40 @@ class Easy{
 
  */
 
+/*Check whether an array is sorted or not.*/
 
+/*
+import java.util.Scanner;
+class Easy{
+    public static boolean check_sorted(int arr[]) {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < arr[i - 1]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of elements in your array:");
+        int n=sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter the elements :");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        if (check_sorted(arr)){
+            System.out.println("The array is sorted.");
+        }else{
+            System.out.println("The array is not sorted.");
+        }
+        sc.close();            
+    }
+}
+
+ */
