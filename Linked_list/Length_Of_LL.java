66170b918ea95ce0f2c96d42d0feda9da4a87463
bsoumya746 <bsoumya746@gmail.com>
2023-12-13 21:23:58 +0530

    Updated

---
+++ Linked_list/Length_Of_LL.java
@@ -0,0 +1,42 @@
+
+
+class Node{
+    int data;
+    Node next;
+
+
+    Node(int data, Node next){
+        this.data = data;
+        this.next=next;
+    }
+
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+
+
+public class Length_Of_LL {
+
+    public static int Length_Of_LL(Node head){
+        int counter=0;
+        Node temp = head;
+        while(temp != null){
+            temp=temp.next;
+            counter++;
+        }
+        return counter;
+    }
+
+    public static void main(String args[]){
+        int arr[] = {2,5,8,7};
+        Node head = new Node(arr[0]);
+        head.next = new Node(arr[1]);
+        head.next.next=new Node(arr[2]);
+        
+        int length = Length_Of_LL(head);
+        System.out.println("The length of the given linked list is:" +length);
+    }
+}
